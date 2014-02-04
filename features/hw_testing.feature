6d83d3d2ce3a169939776bc52c4a3330f9903951
Yaro Apletov <yaro@mail.ru>
2014-02-04 00:27:16 +0000

    more cuc + steps

--- features/hw_testing.feature
+++ features/hw_testing.feature
@@ -3,18 +3,21 @@ Feature: Testing instructor created homeworks
   As a AutoGrader maintainer
   I would like these homeworks to be automatically tested on submit
 
-  Scenario: Runs an AutoGrader grading process on supplied homework
-    Given I have AutoGrader setup
-    And I have a homework "ruby-intro" in the repo
-    When I run the AutoGrader on this homework
-    Then I should see no runtime errors
-
   Scenario: Checks the homeworks file structure
-
   Scenario: Checks that all necessary files are present
-
   Scenario: Checks that the supplied files are of correct format
-
   Scenario: AutoGrader run results are visible in Travis
 
+  Scenario: Runs an AutoGrader grading process on supplied homework
+    Given I have AutoGrader setup
+    Given I have a homework "ruby-intro" in the repo
+    When I run the AutoGrader on this homework
+    Then I should see no runtime errors
+    And I should see test results
+
+  Scenario: Runs AG with provided solution and spec
+  Scenario: Runs AG with provided skeletons and spec
+  Scenario: Runs AG with provided solution an skeleton spec
+
   Scenario: AutoGrader runs test grading process for all homeworks in the repo
+
--- features/step_definitions/hw_testing_steps.rb
+++ features/step_definitions/hw_testing_steps.rb
@@ -1,26 +1,38 @@
+require 'open3'
+
 Given(/^I have a homework "(.*?)" in the repo$/) do |hw_name|
   expect(Dir).to exist(hw_name)
 end
 
 Given /I have AutoGrader setup/ do
+  cli_string = 'cucumber'
+  ENV['BUNDLE_GEMFILE']='Gemfile'
+  puts 'Running cucumber and rspec tests on RAG....'
   Dir.chdir('rag') do
-    puts `cucumber`
+    @test_output, @test_errors, @test_status = Open3.capture3(cli_string)
   end
+
+  expect(@test_status).to be_success
 end
 
 When(/^I run the AutoGrader on this homework$/) do
   test_subject_path = '../ruby-intro/solutions/lib/part1.rb'
   spec_path = '../ruby-intro/autograder/part1_spec.rb'
   cli_string = "./grade #{test_subject_path} #{spec_path}"
+  ENV['BUNDLE_GEMFILE']='Gemfile'
 
   Dir.chdir('rag') do
-    ENV['BUNDLE_GEMFILE']='Gemfile'
-    output = `#{cli_string}`
-    puts output
+    @test_output, @test_errors, @test_status = Open3.capture3(cli_string)
   end
 end
 
-
 Then(/^I should see no runtime errors$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@test_status).to be_success
+end
+
+Then /^I should see test results$/ do
+  # for debugging
+  puts @test_output
+  puts @test_errors
+  puts @test_status
 end
