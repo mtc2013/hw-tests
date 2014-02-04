3a3ed0bdea01d4bc3dd7f0bc7d572989a0f98a82
Paul McCulloch <hrfeels@gmail.com>
2014-02-04 00:56:05 -0800

    added some steps, checking for logs

--- features/step_definitions/hw_testing_steps.rb
+++ features/step_definitions/hw_testing_steps.rb
@@ -1,24 +1,74 @@
 require 'open3'
 
-Given(/^I have a homework "(.*?)" in the repo$/) do |hw_name|
-  expect(Dir).to exist(hw_name)
-end
-
 Given /I have AutoGrader setup/ do
   Dir.chdir('rag') do
+    ENV['BUNDLE_GEMFILE']='Gemfile'
+    puts `bundle install --deployment`
     puts `cucumber`
   end
 end
 
+Given(/^that I am in the project root directory "(.*?)"$/) do |project_dir|
+  @project_dir = project_dir
+  expect(File.basename(Dir.getwd)).to eq @project_dir
+end
+
+And(/^that there is an autograders directory "(.*?)"$/) do |rag|
+ @rag = rag
+end
+
+And(/^that there is a homeworks directory "(.*?)"$/) do |hw|
+ @hw = hw
+end
+
+When(/^I check the autograders directory$/) do
+  expect(Dir).to exist(@rag)
+end
+
+When(/^I delete all the autograder log files$/) do
+  dir_path = Dir.getwd+"/#{@rag}/log"
+  FileUtils.rm_rf("#{dir_path}/.", secure: true)
+  autograder_logs = "#{@rag}/log/*"
+  expect(Dir[autograder_logs]).to be_empty
+end
+
+When(/^I check each homeworks directory$/) do
+  expect(Dir).to exist(@hw)
+  @homeworks = Dir.glob("#{@hw}/*").select {|f| File.directory? f}
+end
+
+Then(/^I should see a directory "(.*?)" with at least one file$/) do |dir_name|
+  @homeworks.each do |hw|
+    dir = "#{hw}/#{dir_name}"
+    expect(Dir).to exist(dir)
+    # requires at least one file with extension in subtree, allows empty folders
+    dir_files = Dir[dir+'/**/*.*']
+    expect(dir_files).not_to be_empty
+  end
+end
+
+Given(/^I have a homework "(.*?)"$/) do |hw_name|
+  expect(Dir).to exist("#{@hw}/#{hw_name}")
+end
+
 When(/^I run the AutoGrader on this homework$/) do
-  test_subject_path = '../ruby-intro/solutions/lib/part1.rb'
-  spec_path = '../ruby-intro/autograder/part1_spec.rb'
+  # AutoGraders must be run from the rag directory because of relative requires
+  rag_to_hw_path = '../hw'
+  test_subject_path = "#{rag_to_hw_path}/ruby-intro/solutions/lib/part1.rb"
+  spec_path         = "#{rag_to_hw_path}/ruby-intro/autograder/part1_spec.rb"
+
   cli_string = "./grade #{test_subject_path} #{spec_path}"
 
     @test_output, @test_errors, @test_status = Open3.capture3(
-        { 'BUNDLE_GEMFILE' => 'Gemfile' }, cli_string, :chdir => './rag'
+        { 'BUNDLE_GEMFILE' => 'Gemfile' }, cli_string, :chdir => 'rag'
     )
 
+  puts @test_output
+
+end
+
+Then(/^I should see an autograder directory "([^"]*)"/) do |dir_name|
+  expect(Dir).to exist(@rag+'/'+dir_name)
 end
 
 Then(/^I should see no runtime errors$/) do
@@ -27,4 +77,20 @@ end
 
 And(/^I should see that the process succeeded$/) do
   expect(@test_status).to be_success
+end
+
+And(/^I should see the test results output$/) do
+  expect(@test_output).not_to eq ''
+end
+
+And(/^I should see a log directory "(.*?)" has (\d+) files$/) do |dir_name, num_files|
+  dir = @rag+'/log/'+dir_name
+  expect(Dir).to exist(dir)
+  expect(Dir[dir+"/*"].length).to have(8).items
+end
+
+And(/^I run cucumber in "rag"$/) do
+  Dir.chdir('rag') do
+    `cucumber`
+  end
 end
